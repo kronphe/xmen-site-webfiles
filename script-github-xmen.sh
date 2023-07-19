effb14253e98967cc739cfd0dc6e8e7d8d307ae1
Roni Konda <99994074+kronphe@users.noreply.github.com>
2023-07-18 20:38:53 -0400

    Update script-github-xmen.sh

--- script-github-xmen.sh
+++ script-github-xmen.sh
@@ -3,7 +3,7 @@ sudo su
 yum update -y
 yum install -y httpd
 cd /var/www/html
-wget https://github.com/kronphe/xmen-site-webfiles/raw/main/xmen-main.zip
+wget https://github.com/kronphe/xmen-site-webfiles/blob/main/xmen-main.zip
 unzip xmen-main.zip
 cp -r /var/www/html/xmen-main/* /var/www/html
 rm -rf xmen-main.zip xmen-main
